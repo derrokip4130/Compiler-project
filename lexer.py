62809cf50a0e08e87f230766a6b6df84068f0e06
Derrick Kiprop <derrickip34@gmail.com>
2024-05-02 12:44:18 +0300

    edit scanner

--- lexer.py
+++ lexer.py
@@ -1,22 +1,5 @@
 import re
-
-reg_expressions = r'".*?"|\b(?:if|else|for|while|print|def|return)\b|\b(?:\d+\.\d+|\d+\.|\.\d+|\d+)\b|\b(?:\w+)\b|[+\-*/=><]|[():;]'
-TOKEN_TYPES = {
-    'STRING': r'".*?"',
-    'KEYWORD': r'\b(?:if|else|for|while|print|def|return)\b',
-    'NUMBER': r'\b(?:\d+\.\d+|\d+\.|\.\d+|\d+)\b',
-    'IDENTIFIER': r'(?!if|else|for|while|print|def|return)([a-zA-Z][a-zA-Z0-9_]*)',
-    'L_BRACKET': r'[(]',
-    'R_BRACKET': r'[)]',
-    'COLON': r'[:]',
-    'SEMI_COLON': r'[;]',
-    'LOGICAL_OPERATOR': r'[<>]|==',
-    'ASSIGNMENT_OPERATOR': r'[=]',
-    'SUM_OPERATOR': r'[+]',
-    'SUB_OPERATOR': r'[-]',
-    'MULT_OPERATOR': r'[*]',
-    'DIV_OPERATOR': r'[/]'
-}
+from reg_ex import reg_expressions, TOKEN_TYPES
 
 identified_tokens = []
 invalid_tokens = []
