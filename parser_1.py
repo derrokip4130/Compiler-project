0494a4f82e3cf8cc30383a6ba4bdaf4d3c349c7a
Derrick Kiprop <derrickip34@gmail.com>
2024-05-13 16:11:27 +0300

    add  a parse tree

--- parser_1.py
+++ parser_1.py
@@ -1,4 +1,13 @@
-from lexer import tokenize, invalid_tokens
+from lexer import tokenize, invalid_tokens 
+
+class Node:
+    def __init__(self, token_type, lexeme):
+        self.token_type = token_type
+        self.lexeme = lexeme
+        self.children = []
+
+    def add_child(self, node):
+        self.children.append(node)
 
 class Parser:
     def __init__(self, tokens):
@@ -8,121 +17,143 @@ class Parser:
 
     def parse(self):
         try:
-            self.program()
+            tree = self.program()
             print("Parsing successful.")
+            return tree
         except SyntaxError as e:
             print(f"Syntax error on line {self.current_token[2]}: {e}")
+            return None
 
     def match(self, token_type):
         if self.current_token[0] == token_type:
+            lexeme = self.current_token[1]
             self.token_index += 1
             if self.token_index < len(self.tokens):
                 self.current_token = self.tokens[self.token_index]
+            return Node(token_type, lexeme)
         else:
             raise SyntaxError(f"Expected {token_type}, found {self.current_token[0]}")
 
     def program(self):
         if self.current_token[1] != 'def':
             raise SyntaxError("Expected 'def' as the first keyword.")
-        self.function_def()
+        tree = Node('PROGRAM', '')
+        tree.add_child(self.function_def())
+        return tree
 
     def function_def(self):
-        self.match('DEF_KEYWORD')
-        self.match('MAIN_KEYWORD')
-        self.match('L_BRACKET') 
-        self.match('R_BRACKET')
-        self.match('COLON')
-        self.statement_list()
+        tree = Node('FUNCTION_DEF', '')
+        tree.add_child(self.match('DEF_KEYWORD'))
+        tree.add_child(self.match('MAIN_KEYWORD'))
+        tree.add_child(self.match('L_BRACKET'))
+        tree.add_child(self.match('R_BRACKET'))
+        tree.add_child(self.match('COLON'))
+        tree.add_child(self.statement_list())
+        return tree
 
     def statement_list(self):
-        self.statement()
+        tree = Node('STATEMENT_LIST', '')
+        tree.add_child(self.statement())
         if self.token_index < len(self.tokens) and self.tokens[self.token_index][1] != 'def':
-            self.statement_list()
+            tree.add_child(self.statement_list())
+        return tree
 
     def statement(self):
+        tree = Node('STATEMENT', '')
         if self.current_token[0] == 'IDENTIFIER':
-            self.assignment()
+            tree.add_child(self.assignment())
         elif self.current_token[1] == 'print':
-            self.print_statement()
+            tree.add_child(self.print_statement())
         elif self.current_token[1] == 'if':
-            self.conditional()
+            tree.add_child(self.conditional())
         elif self.current_token[1] == 'while':
-            self.loop()
+            tree.add_child(self.loop())
         elif self.current_token[1] == 'return':
-            self.return_statement()
+            tree.add_child(self.return_statement())
         else:
             raise SyntaxError(f"Invalid statement: {self.current_token}")
+        return tree
 
     def assignment(self):
-        self.match('IDENTIFIER')
-        self.match('ASSIGNMENT_OPERATOR')
-        self.expression()
+        tree = Node('ASSIGNMENT', '')
+        tree.add_child(self.match('IDENTIFIER'))
+        tree.add_child(self.match('ASSIGNMENT_OPERATOR'))
+        tree.add_child(self.expression())
+        return tree
 
     def conditional(self):
-        self.match('IF_KEYWORD')
-        self.expression()
-        self.match('COLON')
-        self.statement_list()
+        tree = Node('CONDITIONAL', '')
+        tree.add_child(self.match('IF_KEYWORD'))
+        tree.add_child(self.expression())
+        tree.add_child(self.match('COLON'))
+        tree.add_child(self.statement_list())
+        return tree
 
     def loop(self):
-        self.match('WHILE_KEYWORD')
-        self.expression()
-        self.match('COLON')
-        self.statement_list()
-    
+        tree = Node('LOOP', '')
+        tree.add_child(self.match('WHILE_KEYWORD'))
+        tree.add_child(self.expression())
+        tree.add_child(self.match('COLON'))
+        tree.add_child(self.statement_list())
+        return tree
+
     def print_statement(self):
-        self.match('PRINT_KEYWORD')
-        self.match('L_BRACKET')
+        tree = Node('PRINT', '')
+        tree.add_child(self.match('PRINT_KEYWORD'))
+        tree.add_child(self.match('L_BRACKET'))
         if self.current_token[0] == 'STRING':
-            self.match('STRING')
+            tree.add_child(self.match('STRING'))
         elif self.current_token[0] == 'IDENTIFIER':
-            self.match('IDENTIFIER')
-        self.match('R_BRACKET')
-        self.statement_list()
+            tree.add_child(self.match('IDENTIFIER'))
+        tree.add_child(self.match('R_BRACKET'))
+        return tree
 
     def return_statement(self):
-        self.match('RET_KEYWORD')
+        tree = Node('RETURN', '')
+        tree.add_child(self.match('RET_KEYWORD'))
         if self.current_token[0] == 'INTEGER' and self.current_token[1] == '0':
-            self.match('INTEGER')
+            tree.add_child(self.match('INTEGER'))
+        return tree
 
     def expression(self):
-        self.term()
+        tree = Node('EXPRESSION', '')
+        tree.add_child(self.term())
         if self.current_token[0] in 'LOGICAL_OPERATOR' and self.current_token[1] in ['<', '>', '==']:
-            self.match(self.current_token[0])
-            self.term()
+            tree.add_child(self.match(self.current_token[0]))
+            tree.add_child(self.term())
+        return tree
 
     def term(self):
-        self.factor()
+        tree = Node('TERM', '')
+        tree.add_child(self.factor())
         if self.current_token[0] in ['SUM_OPERATOR', 'SUB_OPERATOR', 'DIV_OPERATOR', 'MULT_OPERATOR'] and self.current_token[1] in ['+', '-', '*', '/']:
-            self.match(self.current_token[0])
-            self.factor()
+            tree.add_child(self.match(self.current_token[0]))
+            tree.add_child(self.factor())
+        return tree
 
     def factor(self):
+        tree = Node('FACTOR', '')
         if self.current_token[0] == 'IDENTIFIER':
-            self.match('IDENTIFIER')
+            tree.add_child(self.match('IDENTIFIER'))
         elif self.current_token[0] == 'INTEGER':
-            self.match('INTEGER')
+            tree.add_child(self.match('INTEGER'))
         elif self.current_token[0] == 'FLOAT':
-            self.match('FLOAT')
+            tree.add_child(self.match('FLOAT'))
         elif self.current_token[0] == 'STRING':
-            self.match('STRING')
+            tree.add_child(self.match('STRING'))
         else:
             raise SyntaxError(f"Invalid factor: {self.current_token}")
-        
+        return tree
+    
+    def print_tree(self, node, depth=0):
+        if node:
+            print('  ' * depth + f'{node.token_type}: {node.lexeme}')
+            for child in node.children:
+                self.print_tree(child, depth + 1)
 
 if __name__ == '__main__':
-    file_path = 'new.txt'
-    tokens = tokenize(file_path)
-    print("\nTOKENS\n")
-    for token in tokens:
-        print(f"{token[0]}: {token[1]} on line {token[2]}")
-    if invalid_tokens:
-        print("\n")
-        for invalid_token in invalid_tokens:
-            print(invalid_token)
+    tokens = tokenize("sample_code.txt")
     parser = Parser(tokens)
-    print("\n")
-    try:
-        parser.parse()
-    except SyntaxError as e:
-        print(f"Syntax error: {e}")
+    parse_tree = parser.parse()
+    if parse_tree:
+        parser.print_tree(parse_tree)
